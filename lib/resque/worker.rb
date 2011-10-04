9c256280000bd682a63ce5d351547f08e7eaac77
Terence Lee <hone02@gmail.com>
2011-10-04 16:57:44 -0500

    make only 1 call to redis to prune dead workers

--- lib/resque/worker.rb
+++ lib/resque/worker.rb
@@ -344,10 +344,10 @@ module Resque
     # By checking the current Redis state against the actual
     # environment, we can determine if Redis is old and clean it up a bit.
     def prune_dead_workers
-      all_workers = Worker.all
-      known_workers = worker_pids unless all_workers.empty?
-      all_workers.each do |worker|
-        next if redis.get(worker)
+      all_workers  = Array(redis.smembers(:workers))
+      dead_workers = redis.mget(*all_workers) - all_workers
+      dead_workers.each do |worker_id|
+        worker = Worker.find(worker_id)
         log! "Pruning dead worker: #{worker}"
         worker.unregister_worker
       end
