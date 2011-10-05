934d4c6ae45174a3475b59964197f7e23347c394
Terence Lee <hone02@gmail.com>
2011-10-05 01:30:44 -0500

    make sure expire gets set on key

--- lib/resque/worker.rb
+++ lib/resque/worker.rb
@@ -108,8 +108,10 @@ module Resque
     def setup_keepalive_thread(interval)
       @keepalive_thread = Thread.new {
         loop do
-          redis.set(self, self)
-          redis.expire(self, interval)
+          redis.multi do
+            redis.set(self, self)
+            redis.expire(self, interval)
+          end
           sleep interval
         end
       }
